224464a03dc79c33becf55d651c2007d737469d6
Sonicof <116839500+Sonicof@users.noreply.github.com>
2023-10-18 14:38:44 +0530

    Create valis_sudoku_2.c

---
+++ valis_sudoku_2.c
@@ -0,0 +1,95 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+void main() 
+{ 
+    int n;
+    char arr[9][9][5]= 
+                       { 
+                       {"5","3","4","6","7","8","9","1","2"}, 
+                       {"6","7","2",".","9","5","3","4","8"}, 
+                       {"1","9","8","3","4","2","5","6","7"}, 
+                       {"8","5","9","7","6","1","4","2","3"}, 
+                       {"4","2",".","8","5","3","7","9","1"}, 
+                       {"7","1","3","9","2","4",".","5","6"}, 
+                       {"9","6","1","5","3","7","2","8","4"}, 
+                       {"2","8","7",".","1","9","6","3","5"}, 
+                       {"3","4","5","2","8","6","1","7","."} 
+                       }; 
+    char str1[12],str2[12];                  
+    for(int i=0;i<9;i++)
+    {
+        strcpy(str1,"");
+        for(int j=0;j<9;j++)
+        {
+            char *ptr=arr[i][j]; 
+            char firstchar=*ptr; 
+            if(firstchar=='.')
+            {
+            	continue;
+            }	
+            ptr=strchr(str1,firstchar);
+            if(ptr != NULL)
+            {
+                n=1;
+                goto the_end;
+            }
+            strcat(str1,arr[i][j]);
+        }
+    }
+    //printf("Rows Verified\n");
+    for(int i=0;i<9;i++)
+    {
+        strcpy(str2,"");
+        for(int j=0;j<9;j++)
+        {
+            char *ptr=arr[j][i]; 
+            char firstchar=*ptr; 
+            if(firstchar=='.')
+            {
+            	continue;
+            }
+            ptr=strchr(str2,firstchar);
+            if(ptr != NULL)
+            {
+                n=1;
+                goto the_end;
+            }
+            strcat(str2,arr[j][i]);
+        }
+    }
+    //printf("Coloumns Verified\n");
+    for(int i=3;i<10;i=i+3)
+    {
+        for(int j=3;j<10;j=j+3)
+        {
+            strcpy(str1,"");
+            for(int i1=i-3;i1<i;i1++)
+            {
+                for(int j1=j-3;j1<j;j1++)
+                {
+                    char *ptr=arr[i1][j1]; 
+                    char firstchar=*ptr; 
+                    if(firstchar=='.')
+		    {
+		    	continue;
+		    }
+                    ptr=strchr(str1,firstchar);
+                    if(ptr != NULL)
+                    {
+                        n=1;
+                        goto the_end;
+                    }
+                    strcat(str1,arr[i1][j1]);
+                }
+            }
+        }
+    }
+    //printf("Grids Verified\n");
+    printf("Accepted\n");
+    the_end:
+        if(n==1)
+        {
+            printf("Rejected\n");
+        }
+}        
