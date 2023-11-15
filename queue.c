7e3883887c86afb4e0b1feb4ebd53cd0bdbc7582
Sonicof <116839500+Sonicof@users.noreply.github.com>
2023-11-15 14:40:16 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define MAXSIZE 5
+#define MAXSIZE 50
 
 int queue[MAXSIZE];
 int front=-1,rear=-1;
+
 int isempty()
 {
   return(front == -1 && rear == -1);  
 }
+
 int isfull()
 {
-    return(front==0 && rear == MAXSIZE-1)
+    return(rear == MAXSIZE-1); //reason : in queue element enters only from back
 }
+
 void enqueue(int a)
 {
     if(isfull())
@@ -30,6 +33,71 @@ void enqueue(int a)
             rear=rear+1;
         }
         queue[rear]=a;
-        printf("%d is Queued");
+        printf("%d is Queued \n",a);
     }
 }
+
+void dequeue()
+{
+    if (isempty()) 
+    {
+        printf("Queue is empty. Cannot dequeue\n");
+    }
+    else
+    {
+        int a=queue[front];
+        if(front==rear)
+        {
+            front=-1;
+            rear=-1;
+        }
+        front=front+1;
+        printf("%d Dequeued \n",a);
+    }
+}
+
+void display() 
+{
+    if (isempty()) 
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Queue elements: ");
+    for (int i = front; i <= rear; i++) 
+    {
+        printf("%d ", queue[i]);
+    }
+    printf("\n");
+}
+
+int main() 
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    display();
+
+    dequeue();
+    display();
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    display();
+
+    dequeue();
+    display();enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    display();
+
+    dequeue();
+    display();
+    
+
+    return 0;
+}
+ 
