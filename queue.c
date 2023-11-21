176807b553c482b53650de5fff4d46dfe9f6f6be
Sonicof <116839500+Sonicof@users.noreply.github.com>
2023-11-21 10:42:35 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,103 +1,91 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define MAXSIZE 50
+#include <stdio.h>
+#include <stdlib.h>
 
-int queue[MAXSIZE];
-int front=-1,rear=-1;
+#define MAX_SIZE 5
 
-int isempty()
-{
-  return(front == -1 && rear == -1);  
+int front = -1;
+int rear = -1;
+int queue[MAX_SIZE];
+
+int isEmpty() {
+    return (front == -1);
 }
 
-int isfull()
-{
-    return(rear == MAXSIZE-1); //reason : in queue element enters only from back
+int isFull() {
+    return (rear == MAX_SIZE - 1);
 }
 
-void enqueue(int a)
-{
-    if(isfull())
-    {
-        printf("Queue is full\n");
-    }
-    else
-    {
-        if(isempty())
-        {
-           front=0;
-           rear=0;
-        }
-        else
-        {
-            rear=rear+1;
-        }
-        queue[rear]=a;
-        printf("%d is Queued \n",a);
-    }
-}
-
-void dequeue()
-{
-    if (isempty()) 
-    {
-        printf("Queue is empty. Cannot dequeue\n");
-    }
-    else
-    {
-        int a=queue[front];
-        if(front==rear)
-        {
-            front=-1;
-            rear=-1;
-        }
-        front=front+1;
-        printf("%d Dequeued \n",a);
-    }
-}
-
-void display() 
-{
-    if (isempty()) 
-    {
-        printf("Queue is empty\n");
+void enqueue(int value) {
+    if (isFull()) {
+        printf("Queue is full. Cannot enqueue %d\n", value);
         return;
     }
+
+    if (isEmpty()) {
+        front = 0;
+    }
+
+    rear++;
+    queue[rear] = value;
+
+    printf("Enqueued: %d\n", value);
+}
+
+int dequeue() {
+    int dequeuedValue;
+
+    if (isEmpty()) {
+        printf("Queue is empty. Cannot dequeue\n");
+        return -1;
+    }
+
+    dequeuedValue = queue[front];
+
+    if (front == rear) {
+        front = -1;
+        rear = -1;
+    } else {
+        front++;
+    }
+
+    printf("Dequeued: %d\n", dequeuedValue);
+    return dequeuedValue;
+}
+
+void display() {
+    if (isEmpty()) {
+        printf("Queue is empty.\n");
+        return;
+    }
+
     printf("Queue elements: ");
-    for (int i = front; i <= rear; i++) 
-    {
+    for (int i = front; i <= rear; i++) {
         printf("%d ", queue[i]);
     }
+
     printf("\n");
 }
 
-int main() 
-{
+int main() {
     enqueue(10);
     enqueue(20);
     enqueue(30);
+    enqueue(40);
 
     display();
 
     dequeue();
+    dequeue();
+
+    enqueue(50);
+    enqueue(60);
+
     display();
     enqueue(10);
-    enqueue(20);
-    enqueue(30);
-
-    display();
-
     dequeue();
-    display();enqueue(10);
-    enqueue(20);
-    enqueue(30);
-
-    display();
-
+    dequeue();
     dequeue();
     display();
-    
-
+    dequeue();
     return 0;
 }
- 
