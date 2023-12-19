a2e4c653d1d3d8f7744a268b9ea3650a241b5037
Sonicof <116839500+Sonicof@users.noreply.github.com>
2023-12-20 01:29:44 +0530

    Update splice.pl

--- splice.pl
+++ splice.pl
@@ -1,5 +1,5 @@
 @array=(0..20);
-splice (@array,2,0,99);
+splice (@array,2,2,99);
 print "@array\n";
 @h=join("-",@array);
 print "@h\n";
@@ -9,3 +9,9 @@ print "@array\n";
 print join(" ",reverse @array),"\n";
 @h=reverse @array;
 print "@h\n";
+
+print "\n";
+
+@array=(0..20);
+print join(" ",splice(@array,3,3)),"\n";
+print "@array";
